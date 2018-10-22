8f291b6980185860d084c832c2b9d2b2e39dfe58
DmytroMyloserdov <dagger.dager@gmail.com>
2018-10-22 16:30:45 +0300

    Added for lab files

---
+++ EFModels/CustomDoSomethingStaticClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFModels
+{
+    public static class CustomDoSomethingStaticClass
+    {
+        public static void Map<TRead, TWrite>(TRead read, ref TWrite write) where TRead : class
+                                                                            where TWrite : class
+        {
+            if (read == null)
+            {
+                throw new ArgumentNullException("read");
+            }
+
+            if (write == null)
+            {
+                throw new ArgumentNullException("write");
+            }
+
+            Type readType = typeof(TRead);
+            Type writeType = typeof(TWrite);
+
+
+            foreach (var readProperty in readType.GetProperties())
+            {
+                var writeProperty = writeType.GetProperty(readProperty.Name);
+                if (writeProperty != null)
+                {
+                    if (readProperty.CanRead && writeProperty.CanWrite)
+                    {
+                        var value = readProperty.GetValue(read);
+                        writeProperty.SetValue(write, value);
+                    }
+                }
+            }
+        }
+    }
+}
